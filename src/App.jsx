1ac410f5cd1ec6178cb4d82bac314362d485682e
BorgXQ <jonathan.widarsa@gmail.com>
2023-07-21 18:17:29 +0800

    Add input and result functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { React, useState } from 'react'
-import PersonalInfo from './PersonalInfo'
 import pencilEdit from './assets/pencil-edit.svg'
 import trashCan from './assets/trash-can.svg'
 import './css/main.css'
@@ -10,9 +9,16 @@ function App() {
 
   const [title, setTitle] = useState('')
   const [place, setPlace] = useState('')
-  const [role,setRole] = useState('')
+  const [role, setRole] = useState('')
   const [date, setDate] = useState('')
-  const [desc, setDesc] = useState('')
+  const [textdesc, setTextdesc] = useState('')
+  const [skillset, setSkillset] = useState('')
+
+  const [contents, setContents] = useState([])
+
+  const contentsEdu = contents.filter(content => content.type === 'edu')
+  const contentsWork = contents.filter(content => content.type === 'work')
+  const contentsLead = contents.filter(content => content.type === 'lead')
 
   const changeName = (e) => {
     setName(e.target.value)
@@ -22,6 +28,175 @@ function App() {
     setInfo(e.target.value)
   }
 
+  const changeTitle = (e) => {
+    setTitle(e.target.value)
+  }
+
+  const changePlace = (e) => {
+    setPlace(e.target.value)
+  }
+
+  const changeRole = (e) => {
+    setRole(e.target.value)
+  }
+
+  const changeDate = (e) => {
+    setDate(e.target.value)
+  }
+
+  const changeTextdesc = (e) => {
+    setTextdesc(e.target.value)
+  }
+
+  const changeSkillset = (e) => {
+    setSkillset(e.target.value)
+  }
+
+  const createDetail = (title, place, role, date, textdesc, type) => {
+    return {
+      title: title,
+      place: place,
+      role: role,
+      date: date,
+      textdesc: textdesc,
+      type: type,
+    }
+  }
+
+  const handleSubmitContentEdu = () => {
+    let array = textdesc.split('\n')
+    array = array.map(point => point.trim())
+    setDesc(array)
+
+    let detail = createDetail(title, place, role, date, desc)
+    detail.type = 'edu'
+    setContents(contents => [...contents, detail])
+    setTitle('')
+    setPlace('')
+    setRole('')
+    setDate('')
+    setTextdesc('')
+    setDesc([])
+  }
+
+  const handleSubmitContentWork = () => {
+    let detail = createDetail(title, place, role, date, textdesc)
+    detail.type = 'work'
+    setContents(contents => [...contents, detail])
+    console.log(detail)
+    setTitle('')
+    setPlace('')
+    setRole('')
+    setDate('')
+    setTextdesc('')
+    setDesc([])
+  }
+
+  const handleSubmitContentLead = () => {
+    let array = textdesc.split('\n')
+    array = array.map(point => point.trim())
+    setDesc(array)
+
+    let detail = createDetail(title, place, role, date, desc)
+    detail.type = 'lead'
+    setContents(contents => [...contents, detail])
+    setTitle('')
+    setPlace('')
+    setRole('')
+    setDate('')
+    setTextdesc('')
+    setDesc([])
+  }
+
+  function WriteData() {
+    return (
+      <ul className='data-container'>
+        {contents.map(content => (
+          <li className="data">
+            <img src={pencilEdit} alt="edit"/>
+            <img src={trashCan} alt="delete"/>
+            {content.title}
+          </li>
+        ))}
+      </ul>
+    )
+  }
+
+  function SectionEdu({}) {
+    return (
+      contentsEdu.map(contentEdu => (
+        <>
+        <div className="title-place">
+          <div className="title">{contentEdu.title}</div>
+          <div className="place">{contentEdu.place}</div>
+        </div>
+        <div className="role-date">
+          <div className="role">{contentEdu.role}</div>
+          <div className="date">{contentEdu.date}</div>
+        </div>
+        <ul>
+          {contentEdu.textdesc.split('\n').map(point => (
+            <li><div className="description">{point}</div></li>
+          ))}
+        </ul>
+      </>
+      ))
+    )
+  }
+
+  function SectionWork({}) {
+    return (
+      contentsWork.map(contentWork => (
+        <>
+        <div className="title-place">
+          <div className="title">{contentWork.title}</div>
+          <div className="place">{contentWork.place}</div>
+        </div>
+        <div className="role-date">
+          <div className="role">{contentWork.role}</div>
+          <div className="date">{contentWork.date}</div>
+        </div>
+        <ul>
+          {contentWork.textdesc.split('\n').map(point => (
+            <li><div className="description">{point}</div></li>
+          ))}
+        </ul>
+      </>
+      ))
+    )
+  }
+
+  function SectionLead({}) {
+    return (
+      contentsLead.map(contentLead => (
+        <>
+        <div className="title-place">
+          <div className="title">{contentLead.title}</div>
+          <div className="place">{contentLead.place}</div>
+        </div>
+        <div className="role-date">
+          <div className="role">{contentLead.role}</div>
+          <div className="date">{contentLead.date}</div>
+        </div>
+        <ul>
+          {contentLead.textdesc.split('\n').map(point => (
+            <li><div className="description">{point}</div></li>
+          ))}
+        </ul>
+      </>
+      ))
+    )
+  }
+
+  function SectionSkill() {
+    return (
+      <ul>
+        {skillset.split('\n').map(point => (
+          <li className="skill"><div className="description">{point}</div></li>
+        ))}
+      </ul>
+    )
+  }
 
   return (
     <>
@@ -30,72 +205,22 @@ function App() {
         <p className="personal-info">
             {info}
         </p>
+
         <div className="heading">EDUCATION</div>
         <hr></hr>
-        <div className="title-place">
-          <div className="title">{title}</div>
-          <div className="place">{place}</div>
-        </div>
-        <div className="role-date">
-          <div className="role">{role}</div>
-          <div className="date">{date}</div>
-        </div>
-        <ul>
-          <li><div className="description">{desc}</div></li>
-        </ul>
+        <SectionEdu />
 
         <div className="heading">WORK EXPERIENCE</div>
         <hr></hr>
-        <div className="title-place">
-          <div className="title">Illumina, Inc.</div>
-          <div className="place">Singapore</div>
-        </div>
-        <div className="role-date">
-          <div className="role">Research Intern</div>
-          <div className="date">July 2022 - December 2022</div>
-        </div>
-        <ul>
-          <li><div className="description">Chaired an entire mini-project under senior scientist supervision, studied it and mentored new full-time scientists on my project design within the first half of the program.</div></li>
-          <li><div className="description">Redesigned two major workflows, greatly reducing reagent wastage and increasing time efficiency twofold, which was implemented as project standard in under three months.</div></li>
-          <li><div className="description">Analyzed dozens of research journals and developed formulations that significantly improved the activity and stability of the chemical component of interest.</div></li>
-        </ul>
-
-        <div className="title-place">
-          <div className="title">Self-Employed</div>
-          <div className="place">Singapore</div>
-        </div>
-        <div className="role-date">
-          <div className="role">H2 Mathematics Tutor</div>
-          <div className="date">October 2021 - October 2022</div>
-        </div>
-        <ul>
-          <li><div className="description">Networked effectively with multiple experienced tutors, boosting oneself’s student count up twofold.</div></li>
-          <li><div className="description">Curated a method of pedagogy through students of varying backgrounds, cultivating deeper bonds which sparked their own interest in the subject.</div></li>
-          <li><div className="description">Organized lessons using Google Docs and Sheets, maintaining a balanced and consistent schedule between teaching and other responsibilities.</div></li>
-        </ul>
+        <SectionWork />
 
         <div className="heading">LEADERSHIP EXPERIENCE</div>
         <hr></hr>
-        <div className="title-place">
-          <div className="title">Research Project, School of Biological Sciences</div>
-          <div className="place">Singapore</div>
-        </div>
-        <div className="role-date">
-          <div className="role">Research Director</div>
-          <div className="date">January 2022 - April 2022</div>
-        </div>
-        <ul>
-          <li><div className="description">Reinforced ideas and addressed gaps in previous studies on the effect of lo-fi music on memory-related tasks.</div></li>
-          <li><div className="description">Presented findings to an audience of research students and professors, spurring debates that launched novel links between music and behavioral neurology.</div></li>
-          <li><div className="description">Collaborated with a relevant scientist to mentor the research direction, fortifying the credibility of the research findings.</div></li>
-        </ul>
+        <SectionLead />
 
         <div className="heading">SKILLS</div>
         <hr></hr>
-        <ul>
-          <li className="skill"><div className="description">Proficient in English and Indonesian, conversant in Japanese.</div></li>
-          <li className="skill"><div className="description">Western Blotting | Cell Culture | Gel Electrophoresis | High-Performance Liquid Chromatography | Next-Generation Sequencing | Microplate Reading | Lyophilization | Microsoft Office</div></li>
-        </ul>
+        <SectionSkill />
       </div>
 
       <div className="name-pers-info">
@@ -118,49 +243,61 @@ function App() {
 
       <div className="section">
         <div className="sub-section">
-          <label for="" className="insert">
-            Company/Title:
-            <input type="text" className="input-title" placeholder="ABC Pte. Ltd"/>
-          </label>
-          <label for="" className="insert">
-            Location:
-            <input type="text" className="input-location" placeholder="Singapore"/>
-          </label>
+          <Input
+            label='Company/Title:'
+            value={title}
+            onChange={changeTitle}
+            className='input-title'
+            placeholder="ABC Pte. Ltd"
+          />
+          <Input
+            label='Location:'
+            value={place}
+            onChange={changePlace}
+            className='input-location'
+            placeholder="Singapore"
+          />
         </div>
         <div className="sub-section">
-          <label for="" className="insert">
-            Role:
-            <input type="text" className="input-role" placeholder="Senior Software Engineer"/>
-          </label>
-          <label for="" className="insert">
-            Date:
-            <input type="text" className="input-date" placeholder="February 20XX - Present"/>
-          </label>
+          <Input
+            label='Role:'
+            value={role}
+            onChange={changeRole}
+            className='input-role'
+            placeholder="Senior Software Engineer"
+          />
+          <Input
+            label='Date:'
+            value={date}
+            onChange={changeDate}
+            className='input-date'
+            placeholder="February 20XX - Present"
+          />
         </div>
-        <label for="" className="insert">
-          Description:
-          <textarea className="input-desc"placeholder="- Negotiated with relevant sponsorships, saving XX% of expected budget costs."></textarea>
-        </label>
+        <InputTextArea
+          label='Description:'
+          value={textdesc}
+          onChange={changeTextdesc}
+          className='input-desc'
+          placeholder={'Negotiated...., saving XX% of expected budget costs.\nCollaborated..., achieving a company growth of XX% in XX years.'}
+          />
       </div>
 
       <div className="btn-container">
-        <button className="to-edu">Education</button>
-        <button className="to-work">Work Experience</button>
-        <button className="to-leader">Leadership Experience</button>
+        <button className="to-edu" onClick={handleSubmitContentEdu}>Education</button>
+        <button className="to-work" onClick={handleSubmitContentWork}>Work Experience</button>
+        <button className="to-leader" onClick={handleSubmitContentLead}>Leadership Experience</button>
       </div>
 
-      <ul class="data-container">
-        <li class="data">
-          Nanyang Technological University
-          <img src={pencilEdit} alt="edit"/>
-          <img src={trashCan} alt="delete"/>
-        </li>
-      </ul>
+      <WriteData />
 
-      <label for="" className="insert skill-section">
-        Skills:
-        <textarea className="input-skill" placeholder="- Javascript | HTML | CSS | React | Node.js | Express | MongoDB"></textarea>
-      </label>
+      <InputSkillArea
+        label='Skills:'
+        value={skillset}
+        onChange={changeSkillset}
+        className='input-skill'
+        placeholder={'Proficient in English, conversant in Japanese.\nJavascript | HTML | CSS | React | Node.js | Express | MongoDB'}
+      />
     </>
   )
 }
@@ -170,7 +307,8 @@ function Input({
   value,
   onChange,
   className,
-  placeholder}) {
+  placeholder
+}) {
   return (
     <label className='insert'>
       {label}
@@ -180,6 +318,46 @@ function Input({
         className={className}
         placeholder={placeholder}
       />
+    </label>
+  )
+}
+
+function InputTextArea({
+  label,
+  value,
+  onChange,
+  className,
+  placeholder
+}) {
+  return (
+    <label className='insert'>
+      {label}
+      <textarea
+        value={value}
+        onChange={onChange}
+        className={className}
+        placeholder={placeholder}
+      ></textarea>
+    </label>
+  )
+}
+
+function InputSkillArea({
+  label,
+  value,
+  onChange,
+  className,
+  placeholder
+}) {
+  return (
+    <label className='insert skill-section'>
+      {label}
+      <textarea
+        value={value}
+        onChange={onChange}
+        className={className}
+        placeholder={placeholder}
+      ></textarea>
     </label>
   )
 }
